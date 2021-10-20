b680cc34c3a6d70fd0957b2bf4899ee4c8b7dfff
German Baena <germanb66@gmail.com>
2021-10-20 00:14:47 -0500

    Codigo temporalmente optimizado

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,14 @@
 			inputs = document.querySelectorAll('.input'),
 			textArea = document.getElementById('text-area')
 
+	// Objeto Expresiones Regulares.
 	const er = {
 		regExpName: /^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/,
 		regExpMail: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 		regExpMessage: /^[a-zA-Z0-9?$@#()'!,+\-=_:.&€£*%\s]+$/
 	}
 
+	// Objeto validar campos.
 	const validarCampo = {
 		nameField: false,
 		mailField: false,
@@ -17,14 +19,12 @@
 	}
 
 
+	// Función validar formulario.
 	const validarFormulario = e => {
 		const mailDos = document.getElementById('confirm-email')
 
 		// Nombre
-		if (e.target.name === 'name') {
-			validarDatos(er.regExpName, e.target.value, e.target)
-			// console.log(e.target)
-		}
+		if (e.target.name === 'name') validarDatos(er.regExpName, e.target.value, e.target)
 
 		// Email
 		if (e.target.name === 'email') {
@@ -33,47 +33,33 @@
 		}
 
 		// Confirm-Email
-		if (e.target.name === 'confirm-email') {
-			validarMail2(e.target)
-		}
+		if (e.target.name === 'confirm-email') validarMail2(e.target)
 
 		// Mensaje
-		if (e.target.name === 'text-area') {
-			validarDatos(er.regExpMessage, e.target.value, e.target)
-		}
+		if (e.target.name === 'text-area') validarDatos(er.regExpMessage, e.target.value, e.target)
 	}
 
 
+	// Función validar datos.
 	const validarDatos = (expresion, valor, elemento ) => {
-		if (elemento.name !== 'text-area') {
-			if (expresion.test(valor)) {
-				changeClass(true, elemento)
-			} else {
-				changeClass(false, elemento)
-			}
-		} else if (expresion.test(valor)) {
-			changeClass(true, elemento)
-		} else {
-			changeClass(false, elemento)
-		}
+			if (expresion.test(valor)) changeClass(true, elemento)
+			else changeClass(false, elemento)
 	}
 
 
+	// Función confirmar correo.
 	const validarMail2 = elemento => {
 		const 	inputEmail = document.getElementById('email'),
 				inputEmail2 = document.getElementById('confirm-email')
 
 		if (inputEmail.value !== '') {
-			if (inputEmail.value === inputEmail2.value) {
-				changeClass(true, elemento)
-			} else {
-				changeClass(false, elemento)
-			}
-		} else {
-			changeClass(false, elemento)
-		}
+			if (inputEmail.value === inputEmail2.value) changeClass(true, elemento)
+			else changeClass(false, elemento)
+		} else changeClass(false, elemento)
 	}
 
+
+	// Función añadir o quitar clases.
 	const changeClass = (condicion, elemento) => {
 		if (condicion) {
 			elemento.classList.remove('incorrecto', 'txarea-incorrecto')
@@ -84,9 +70,13 @@
 		}
 	}
 
+
+	// Evento text-area.
 	textArea.addEventListener('keyup', validarFormulario)
 	textArea.addEventListener('blur', validarFormulario)
 
+
+	// Evento inputs.
 	inputs.forEach(input => {
 		input.addEventListener('keyup', validarFormulario)
 		input.addEventListener('blur', validarFormulario)
