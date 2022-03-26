07db3507357f81a8ebf6d7117aa342efd22d6634
German Baena <germanb66@gmail.com>
2022-03-25 19:02:49 -0500

    Se incluyo el marcado html para la ventana modal

--- js/scripts.js
+++ js/scripts.js
@@ -121,5 +121,21 @@ inputs.forEach(input => {
 })
 
 
+form.addEventListener('submit', handleSubmit)
 
+async function handleSubmit(event) {
+	event.preventDefault()
+	const $form = new FormData(this)
+	const response = await fetch(this.action, {
+		method: this.method,
+		body: $form,
+		headers: {
+			'Accept': 'application/json'
+		}
+	})
+	if (response.ok) {
+		this.reset()
+		alert('Formulario enviado exitosamente')
+	}
+}
 
